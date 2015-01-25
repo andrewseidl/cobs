b3920168bd56c71a5bf432ed9cc5c60b35e767e5
Andrew Seidl <dev@aas.io>
2015-01-24 22:05:02 -0600

    Add initial instrumenter

---
+++ instrumenter/instrumenter.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"archive/tar"
+	"bytes"
+	"log"
+)
+
+func main() {
+	buf := new(bytes.Buffer)
+	tw := tar.NewWriter(buf)
+
+	var files = []struct {
+		Name, Body string
+	}{
+		{"Dockerfile.txt", "FROM debian"},
+	}
+
+	for _, file := range files {
+		hdr := &tar.Header{
+			Name: file.Name,
+			Size: int64(len(file.Body)),
+		}
+
+		if err := tw.WriteHeader(hdr); err != nil {
+			log.Fatalln(err)
+		}
+		if _, err := tw.Write([]byte(file.Body)); err != nil {
+			log.Fatalln(err)
+		}
+	}
+	if err := tw.Close(); err != nil {
+		log.Fatalln(err)
+	}
+
+}
